e5bc3982ce478ca7fa89287c1e17b2e729798443
Arine <peppydays@gmail.com>
2025-12-14 05:19:47 +0900

    FF0

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 use std::io;
 use std::io::Write;
+use std::process::exit;
 
 fn main() {
+    loop {
+        prompt();
+        let command = read();
+        if command.trim() == "exit" {
+            break;
+        }
+        execute(command);
+    }
+}
+
+fn prompt() {
     print!("$ ");
     io::stdout().flush().unwrap();
+}
 
+fn read() -> String {
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
+    command
+}
 
-    println!("{}: command not found", command.trim());
+fn execute(command: String) {
+    let command = command.trim();
+
+    match command {
+        "exit" => {
+            exit(0);
+        }
+        _ => {
+            println!("{}: command not found", command);
+        }
+    }
 }
