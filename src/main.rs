b1e789645ff506f4dde0e3a5c2d5616be7fe33f7
Arine <peppydays@gmail.com>
2025-12-16 02:56:49 +0900

    YT5

--- src/main.rs
+++ src/main.rs
@@ -101,14 +101,22 @@ fn parse_args(input: &str) -> Vec<String> {
     let mut current = String::new();
     let mut in_double_quotes = false;
     let mut in_single_quotes = false;
+    let mut in_escape = false;
 
     for c in input.chars() {
         match c {
-            c if c == '\'' && !in_double_quotes => {
+            c if c == '\'' && !in_double_quotes && !in_escape => {
                 in_single_quotes = !in_single_quotes;
             }
-            c if c == '\"' && !in_single_quotes => {
+            c if c == '\"' && !in_single_quotes && !in_escape => {
                 in_double_quotes = !in_double_quotes;
+            }
+            c if c == '\\' && !in_single_quotes && !in_double_quotes => {
+                in_escape = true;
+            }
+            c if in_escape => {
+                current.push(c);
+                in_escape = false;
             }
             c if c.is_whitespace() && !in_single_quotes && !in_double_quotes => {
                 if !current.is_empty() {
